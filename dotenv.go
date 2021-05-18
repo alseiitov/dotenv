ea6c5fd639bda58b53152671d29dadfb1eb3653b
alseiitov <dossan.alseiit@gmail.com>
2021-05-18 16:30:29 +0600

    add file

---
+++ dotenv.go
@@ -0,0 +1,29 @@
+package dotenv
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func Load() {
+	bytes, err := ioutil.ReadFile(".env")
+	if err != nil {
+		log.Fatalf("error parsing file: %v", err.Error())
+	}
+
+	lines := strings.Split(string(bytes), "\n")
+
+	for i, line := range lines {
+		if len(line) > 0 && !strings.HasPrefix(line, "#") {
+			arr := strings.Split(line, "=")
+			if len(arr) != 2 {
+				log.Fatalf("invalid format at line %v\n%v", i+1, line)
+			}
+
+			key, value := arr[0], arr[1]
+			os.Setenv(key, value)
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alseiitov/dotenv
+
+go 1.15
